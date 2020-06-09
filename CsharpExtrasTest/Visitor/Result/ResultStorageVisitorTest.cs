49577c62d05d1c51c449d76649ec3654aac12a0e
cbhandal <bhandalc@gmail.com>
2020-06-09 15:40:24 +0100

    Added compile-time test for result storage visitor
    Added this as part of a new test class which can also be used for regular tests later

---
+++ CsharpExtrasTest/Visitor/Result/ResultStorageVisitorTest.cs
@@ -0,0 +1,48 @@
+﻿using CsharpExtras.Visitor;
+using CsharpExtras.Visitor.Result;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpExtrasTest.Visitor.Result
+{
+    [TestFixture]
+    class ResultStorageVisitorTest
+    {
+    }
+
+    #region Compile Time Test
+    //Compile time tests are code to test our types via the compiler. The code doesn't need to be run.
+    class MockVisitable : IMockVisitable
+    {
+        public void Accept(IVisitorBase<IMockVisitable> visitor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal interface IMockVisitable : IVisitableBase<IMockVisitable>
+    {
+    }
+
+    class MockStorageVisitor : ResultStorageVisitorBase<IMockVisitable, IMockType>, IMockStorageVisitor
+    {
+        protected override IMockType DefaultResult()
+        {
+            return new MockClass();
+        }
+    }
+
+    internal class MockClass : IMockType { }
+
+    internal interface IMockType
+    {
+    }
+
+    internal interface IMockStorageVisitor
+    {
+    }
+
+    #endregion
+}
