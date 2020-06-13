781e2a6dc3f3bb3735cc4fa03014848558682eeb
ilusi0n <ha.cabrita@gmail.com>
2020-06-13 11:39:13 +0100

    use explicit types

--- CsharpExtrasTest/Extensions/BoolExtensionTest.cs
+++ CsharpExtrasTest/Extensions/BoolExtensionTest.cs
@@ -10,7 +10,7 @@ namespace CsharpExtrasTest.Extensions
         [Category("Unit")]
         public void Given_Yes_String_When_IsYes_Then_True_Returned()
         {
-            var result = BoolExtension.IsYes("Yes");
+            bool result = BoolExtension.IsYes("Yes");
             Assert.IsTrue(result);
         }
 
@@ -18,7 +18,7 @@ namespace CsharpExtrasTest.Extensions
         [Category("Unit")]
         public void Given_Random_String_When_IsYes_Then_True_Returned()
         {
-            var result = BoolExtension.IsYes("fake");
+            bool result = BoolExtension.IsYes("fake");
             Assert.IsFalse(result);
         }
 
@@ -26,7 +26,7 @@ namespace CsharpExtrasTest.Extensions
         [Category("Unit")]
         public void Given_No_String_When_IsYes_Then_False_Returned()
         {
-            var result = BoolExtension.IsYes("No");
+            bool result = BoolExtension.IsYes("No");
             Assert.False(result);
         }
 
@@ -34,7 +34,7 @@ namespace CsharpExtrasTest.Extensions
         [Category("Unit")]
         public void Given_Yes_String_When_TryParseYesNo_Then_True_Returned_Result_True_Returned()
         {
-            var response = BoolExtension.TryParseYesNo("Yes", out var result);
+            bool response = BoolExtension.TryParseYesNo("Yes", out bool result);
             Assert.IsTrue(result);
             Assert.IsTrue(response);
         }
@@ -43,7 +43,7 @@ namespace CsharpExtrasTest.Extensions
         [Category("Unit")]
         public void Given_No_String_When_TryParseYesNo_Then_True_Returned_Result_False_Returned()
         {
-            var response = BoolExtension.TryParseYesNo("No", out var result);
+            bool response = BoolExtension.TryParseYesNo("No", out bool result);
             Assert.IsTrue(response);
             Assert.IsFalse(result);
         }
@@ -52,7 +52,7 @@ namespace CsharpExtrasTest.Extensions
         [Category("Unit")]
         public void Given_Random_String_When_TryParseYesNo_Then_False_Returned_Result_False_Returned()
         {
-            var response = BoolExtension.TryParseYesNo("Fake", out var result);
+            bool response = BoolExtension.TryParseYesNo("Fake", out bool result);
             Assert.IsFalse(response);
             Assert.IsFalse(result);
         }
@@ -61,7 +61,7 @@ namespace CsharpExtrasTest.Extensions
         [Category("Unit")]
         public void Given_True_When_ToYesNoString_Then_Yes_String_Returned()
         {
-            var response = true.ToYesNoString();
+            string response = true.ToYesNoString();
             Assert.AreEqual("Yes", response);
         }
 
@@ -69,7 +69,7 @@ namespace CsharpExtrasTest.Extensions
         [Category("Unit")]
         public void Given_False_When_ToYesNoString_Then_No_String_Returned()
         {
-            var response = false.ToYesNoString();
+            string response = false.ToYesNoString();
             Assert.AreEqual("No", response);
         }
     }
