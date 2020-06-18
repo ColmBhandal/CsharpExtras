cc022d08b56a5327365c0fc6efdd4cca2e2ac4d5
cbhandal <bhandalc@gmail.com>
2020-06-18 10:17:29 +0100

    Added some documentation on how the abstract validated class works

--- CsharpExtras/ValidatedType/Validated.cs
+++ CsharpExtras/ValidatedType/Validated.cs
@@ -4,6 +4,14 @@ using System.Text;
 
 namespace CsharpExtras.ValidatedType
 {
+    /// <summary>
+    /// A wrapper class for a generically typed object which validates that object upon construction.
+    /// The reference to the wrapped object cannot change, meaning if the object is immutable, then so is this class.
+    /// More generally, if the validation condition represents an invariant on the wrapper object, then it will always hold
+    /// for the liftetime of the validated object.
+    /// The validation condition is abstract in this class, meaning it must be implemented for a sub-class.
+    /// It is up to the architect of the sub-class to choose an appropriate validation condition.
+    /// </summary>    
     public abstract class Validated<TVal>
     {
         private readonly TVal _value;
