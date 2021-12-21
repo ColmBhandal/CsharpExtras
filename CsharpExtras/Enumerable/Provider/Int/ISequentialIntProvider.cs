463041159193b81fc650bfc03819276561354c1f
cbhandal <bhandalc@gmail.com>
2021-12-21 16:37:15 +0000

    Added documentation to interface

--- CsharpExtras/Enumerable/Provider/Int/ISequentialIntProvider.cs
+++ CsharpExtras/Enumerable/Provider/Int/ISequentialIntProvider.cs
@@ -1,10 +1,21 @@
 ﻿namespace CsharpExtras.Enumerable.Provider.Int
 {
+    /// <summary>
+    /// Provides iteration over an arithmetic sequence of ints, starting at the given value & increasing by the given step
+    /// </summary>
     public interface ISequentialIntProvider
     {
+        /// <summary>
+        /// Value to start at
+        /// </summary>
         int Start { get; }
-        int Step { get; }
 
+        /// <summary>
+        /// Value by which to increase the sequence at each successive term
+        /// </summary>
+        int Step { get; }
+        
+        /// <returns>The next successive term in the sequence</returns>
         int Next();
     }
 }
