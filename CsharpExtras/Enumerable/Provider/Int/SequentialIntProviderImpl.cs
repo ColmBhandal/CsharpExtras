2a62e7370c71dafd11fdde3c11e384ea9d197320
cbhandal <bhandalc@gmail.com>
2021-12-21 11:27:59 +0000

    Implemented provider to make tests pass

--- CsharpExtras/Enumerable/Provider/Int/SequentialIntProviderImpl.cs
+++ CsharpExtras/Enumerable/Provider/Int/SequentialIntProviderImpl.cs
@@ -9,15 +9,50 @@ namespace CsharpExtras.Enumerable.Provider.Int
         public int Start { get; }
         public int Step { get; }
 
+        private int _current;
+
         public SequentialIntProviderImpl(int start, int step)
         {
             Start = start;
             Step = step;
+            _current = start;
         }
 
         public int Next()
         {
-            return 1;
+            AssertNextIsInBounds();
+            _current += Step;
+            return _current;
+        }
+
+        private void AssertNextIsInBounds()
+        {
+            AssertNextLeqIntMax();
+            AssertNextGeqIntMin();
+        }
+
+        private void AssertNextGeqIntMin()
+        {
+            if (Step < 0)
+            {
+                int MinAllowableCurrent = int.MinValue - Step;
+                if (_current < MinAllowableCurrent)
+                {
+                    throw new IndexOutOfRangeException($"Current index {_current} plus step {Step} will exceed int Min value");
+                }
+            }
+        }
+
+        private void AssertNextLeqIntMax()
+        {
+            if(Step > 0)
+            {
+                int maxAllowableCurrent = int.MaxValue - Step;
+                if(_current > maxAllowableCurrent)
+                {
+                    throw new IndexOutOfRangeException($"Current index {_current} plus step {Step} will exceed int Max value");
+                }
+            }
         }
     }
 }
