62623adf129246b588a3c2569c6815ff7ef59707
cbhandal <bhandalc@gmail.com>
2022-01-19 16:20:10 +0000

    Skeletal sparse array class

---
+++ CsharpExtras/Map/Sparse/SparseArrayImpl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpExtras.Map.Sparse
+{
+    internal class SparseArrayImpl<TVal>
+    {
+        /// <summary>
+        /// This class encapsulates validated types which are validated according to the range validation functions
+        /// Through private encapsulation of the underlying constructor, the class only exposes indexes that are valid
+        /// </summary>
+        private class ValidIndex
+        {
+        }
+    }
+}
