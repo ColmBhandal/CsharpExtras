aa9aea47821091b2e0e093a5c44349a6aa1f7b82
cbhandal <bhandalc@gmail.com>
2022-01-20 09:41:47 +0000

    Sparse array: Interface Documentation & renaming

--- CsharpExtras/Map/Sparse/SparseArrayImpl.cs
+++ CsharpExtras/Map/Sparse/SparseArrayImpl.cs
@@ -1,4 +1,5 @@
 ﻿using CsharpExtras.Api;
+using CsharpExtras.Compare;
 using CsharpExtras.Map.Dictionary.Curry;
 using CsharpExtras.ValidatedType;
 using CsharpExtras.ValidatedType.Numeric.Integer;
@@ -8,42 +9,57 @@ using System.Text;
 
 namespace CsharpExtras.Map.Sparse
 {
-    internal class SparseArrayImpl<TVal>
+    internal class SparseArrayImpl<TVal> : ISparseArray<TVal>
     {
         public PositiveInteger Dimension { get; }
+        public TVal DefaultValue { get; }
+
+        //TODO: Implement
+        public TVal this[params int[] coordinates]
+        {
+            get => DefaultValue;
+            set { return; }
+        }
 
         private readonly ICurryDictionary<ValidIndex, TVal> _backingDictionary;
         private readonly ICsharpExtrasApi _api;
-        
+
         private readonly Func<int, int, bool> _validationFunction;
 
         private ILazyFunctionMap<(int index, int dimension), SparseArrayImpl<TVal>.ValidIndex> _validIndexCache;
 
         public SparseArrayImpl(int indexBase, PositiveInteger dimension, ICsharpExtrasApi api,
-            Func<int, int, bool> validationFunction)
+            Func<int, int, bool> validationFunction, TVal defaultValue)
         {
             Dimension = dimension ?? throw new ArgumentNullException(nameof(dimension));
+            DefaultValue = defaultValue;
             _api = api ?? throw new ArgumentNullException(nameof(api));
             _validationFunction = validationFunction;
             _backingDictionary = api.NewGenericCurryDictionaryWrapper(
                 api.NewCurryDictionary<int, TVal>(Dimension), KeyInTransform, KeyOutTransform, v => v, v => v);
-            _validIndexCache = api.NewLazyFunctionMap<(int index, int dimension), SparseArrayImpl<TVal>.ValidIndex>
-                ((p) => new ValidIndex(p.index, (PositiveInteger)p.dimension, this));
+            _validIndexCache = api.NewLazyFunctionMap<(int index, int axisIndex), SparseArrayImpl<TVal>.ValidIndex>
+                ((p) => new ValidIndex(p.index, (PositiveInteger)p.axisIndex, this));
         }
 
-        private int KeyInTransform(SparseArrayImpl<TVal>.ValidIndex index, int dimensionIndex) => index;
+        public IComparisonResult CompareUsedValues(ISparseArray<TVal> other, Func<TVal, TVal, bool> valueComparer)
+        {
+            //TODO: Implement properly (this is just a stub)
+            return new CurryDictionaryComparisonImpl<int, int>(1, 1, 1, 1, null);
+        }
 
-        private SparseArrayImpl<TVal>.ValidIndex KeyOutTransform(int index, int dimensionIndex) => _validIndexCache[(index, dimensionIndex)];
+        private int KeyInTransform(SparseArrayImpl<TVal>.ValidIndex index, int axisIndex) => index;
+
+        private SparseArrayImpl<TVal>.ValidIndex KeyOutTransform(int index, int axisIndex) => _validIndexCache[(index, axisIndex)];
 
         private class ValidIndex : ImmutableValidated<int>
         {
-            private readonly PositiveInteger _dimensionIndex;
+            private readonly PositiveInteger _axisIndexIndex;
             private readonly SparseArrayImpl<TVal> _array;
 
-            public ValidIndex(int index, PositiveInteger dimension, SparseArrayImpl<TVal> array) : base(index)
+            public ValidIndex(int index, PositiveInteger axisIndex, SparseArrayImpl<TVal> array) : base(index)
             {
                 _array = array;
-                _dimensionIndex = dimension;
+                _axisIndexIndex = axisIndex;
             }
 
             protected override string ValidityConditionTextDescription =>
